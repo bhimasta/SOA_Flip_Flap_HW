a91c8670f9dacb656fe02b1105c8897addee3037
bhimasta <agoengbhimasta@gmail.com>
2017-09-30 15:31:30 +0800

    Solved the homework and all tests pass

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -1,14 +1,19 @@
 # Module that can be included (mixin) to take and output TSV data
 module TsvBuddy
-  # take_tsv: converts a String with TSV data into @data
+  # take_tsv: String w/ TSV to @data. parameter: tsv - a String in TSV format
   # parameter: tsv - a String in TSV format
   def take_tsv(tsv)
-
+    lines = tsv.split("\n")
+    keys = lines[0].split("\t")
+    @data = (lines[1..-1]).map { |val| Hash[keys.zip(val.split("\t"))] }
   end
 
-  # to_tsv: converts @data into tsv string
+  # to_tsv: converts @data to tsv string.
   # returns: String in TSV format
   def to_tsv
-
+    tsv_files = ''
+    tsv_files << @data[0].keys.join("\t") + "\n"
+    @data.each { |line| tsv_files << line.values.join("\t") + "\n" }
+    tsv_files
   end
 end
