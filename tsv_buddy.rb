e2007841956ea00b1e78e1c491159e72a128889b
bhimasta <agoengbhimasta@gmail.com>
2017-10-01 15:25:53 +0800

    Improve flog score, rubocop tested no offense, and all test passed

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -3,17 +3,38 @@ module TsvBuddy
   # take_tsv: String w/ TSV to @data. parameter: tsv - a String in TSV format
   # parameter: tsv - a String in TSV format
   def take_tsv(tsv)
-    lines = tsv.split("\n")
-    keys = lines[0].split("\t")
-    @data = (lines[1..-1]).map { |val| Hash[keys.zip(val.split("\t"))] }
+    lines = line_splitter(tsv)
+    keys = key_splitter(lines)
+    yml_format(lines, keys)
   end
 
   # to_tsv: converts @data to tsv string.
-  # returns: String in TSV format
+  # returns: String in TSV format (tsv_files)
   def to_tsv
-    tsv_files = ''
+    tsv_format(tsv_files = '')
+    tsv_files
+  end
+
+  private
+
+  # line_spliiter : split into line
+  def line_splitter(tsv)
+    tsv.split("\n")
+  end
+
+  # key_spliiter : split keys in the first row
+  def key_splitter(lines)
+    lines[0].split("\t")
+  end
+
+  # yml_format : process converting tsv lines to @data
+  def yml_format(lines, keys)
+    @data = (lines[1..-1]).map { |val| Hash[keys.zip(val.split("\t"))] }
+  end
+
+  # tsv_converter: process converting @data to tsv string
+  def tsv_format(tsv_files)
     tsv_files << @data[0].keys.join("\t") + "\n"
     @data.each { |line| tsv_files << line.values.join("\t") + "\n" }
-    tsv_files
   end
 end
